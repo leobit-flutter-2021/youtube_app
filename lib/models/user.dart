dc03d99231a8342338103db688541110913d4225
maksymbudzin <maxi985049@gmail.com>
2021-10-19 15:55:45 +0300

    Added profile screen

---
+++ lib/models/user.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UserProfile {
+  String username;
+  AssetImage profilePicture;
+
+  UserProfile({
+    required this.username,
+    required this.profilePicture
+  });
+}
